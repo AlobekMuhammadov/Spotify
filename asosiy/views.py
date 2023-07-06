147b72963423942deb27a1e3baf689573f4cc034
AlobekMuhammad <alobekmuhammadov1@gmail.com>
2023-07-06 23:30:16 +0500

    uygavazifa

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, filters
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from .serializer import *
 
-class ArtistlarAPIView(APIView):
-    def get(self,request):
-        artists = Qoshiqchi.objects.all()
-        serializer = QoshiqchiSerializer(artists,many=True)
-        return Response(serializer.data,status=status)
+class ArtistModelViewSet(ModelViewSet):
+    queryset = Qoshiqchi.objects.all()
+    serializer_class = QoshiqchiSerializer
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['ism','davlat']
+    ordering_fields = ['tugilgan_yil']
 
 
-    def post(self,request):
-        malumot = request.data
-        serializer = QoshiqchiSerializer(data=malumot)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ArtistDetail(APIView):
     def put(self,request,pk):
@@ -35,6 +31,39 @@ class ArtistDetail(APIView):
     def delete(self,request,pk):
         Qoshiqchi.objects.get(id=pk).delete()
         return Response(status=status.HTTP_100_CONTINUE)
+
+
+class QoshiqModelViewSet(ModelViewSet):
+    queryset = Qoshiq.objects.all()
+    serializer_class = QoshiqSerializer
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['nom','janr']
+    ordering_fields = ['davomiylik']
+class AlbomModelViewSet(ModelViewSet):
+    queryset = Albom.objects.all()
+    serializer_class = AlbomSerializer
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['nom']
+    ordering_fields = ['sana']
+
+    @action(detail=True,methods=['GET','POST'])
+    def qoshiqlar(self,request,pk):
+        if request.method == 'POST':
+            qoshiq = request.data
+            albom = self.get_object()
+            serializer = QoshiqSerializer(data=qoshiq)
+            if serializer.is_valid():
+                serializer.save()
+                oxirgi = Qoshiq.objects.last()
+                albom.aktyorlar.add(oxirgi)
+                albom.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        albom = self.get_object()
+        qoshiqs = albom.qoshiq.all()
+        serializer = QoshiqSerializer(qoshiqs,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 
 
